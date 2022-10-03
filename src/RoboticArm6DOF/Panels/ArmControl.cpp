872280e0f1d744cad74d3867606afbc038b5fa13
TheReaper2077 <krishnadev7777@gmail.com>
2022-10-03 23:47:35 +0530

    arrag

--- src/RoboticArm6DOF/Panels/ArmControl.cpp
+++ src/RoboticArm6DOF/Panels/ArmControl.cpp
@@ -10,27 +10,40 @@ static auto& registry = Registry::Instance();
 static auto treenode_flags = ImGuiTreeNodeFlags_Bullet | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_DefaultOpen;
 
 void lra::panel::Controller() {
-	auto& controller = registry.store<lra::Controller>();
+	// auto& controller = registry.store<lra::Controller>();
 
-	if (ImGui::Begin("Controller", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize)) {
-		static bool is_slider = true;
+	// if (ImGui::Begin("Controller", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize)) {
+	// 	static bool is_slider = true;
 
-		ImGui::SliderDragFloat("Base", &controller.target_joint_angles.base, 0.1, 0, 180, is_slider);
-		ImGui::SliderDragFloat("Arm", &controller.target_joint_angles.arm, 0.1, 0, 180, is_slider);
-		ImGui::SliderDragFloat("Elbow", &controller.target_joint_angles.elbow, 0.1, 0, 180, is_slider);
-		ImGui::SliderDragFloat("Wrist", &controller.target_joint_angles.wrist, 0.1, 0, 180, is_slider);
+	// 	ImGui::SliderDragFloat("Base", &controller.target_joint_angles.base, 0.1, 0, 180, is_slider);
+	// 	ImGui::SliderDragFloat("Arm", &controller.target_joint_angles.arm, 0.1, 0, 180, is_slider);
+	// 	ImGui::SliderDragFloat("Elbow", &controller.target_joint_angles.elbow, 0.1, 0, 180, is_slider);
+	// 	ImGui::SliderDragFloat("Wrist", &controller.target_joint_angles.wrist, 0.1, 0, 180, is_slider);
 
-		ImGui::SliderDragFloat("Gripper Rotation", &controller.target_joint_angles.gripper_rotate, 0.1, 0, 180, is_slider);
-		ImGui::SliderDragFloat("Gripper Control", &controller.target_joint_angles.gripper_control, 0.1, 0, 180, is_slider);
-	}
+	// 	ImGui::SliderDragFloat("Gripper Rotation", &controller.target_joint_angles.gripper_rotate, 0.1, 0, 180, is_slider);
+	// 	ImGui::SliderDragFloat("Gripper Control", &controller.target_joint_angles.gripper_control, 0.1, 0, 180, is_slider);
+	// }
 
-	ImGui::End();
+	// ImGui::End();
 }
 
 void lra::panel::Kinematics() {
 	auto& controller = registry.store<lra::Controller>();
 
-	if (ImGui::Begin("Kinematics", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize)) {
+	if (ImGui::Begin("RoboticArm", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize)) {
+		if (ImGui::TreeNodeEx("Joint Angles", treenode_flags)) {
+			static bool is_slider = true;
+
+			ImGui::SliderDragFloat("Base", &controller.target_joint_angles.base, 0.1, 0, 180, is_slider);
+			ImGui::SliderDragFloat("Arm", &controller.target_joint_angles.arm, 0.1, 0, 180, is_slider);
+			ImGui::SliderDragFloat("Elbow", &controller.target_joint_angles.elbow, 0.1, 0, 180, is_slider);
+			ImGui::SliderDragFloat("Wrist", &controller.target_joint_angles.wrist, 0.1, 0, 180, is_slider);
+
+			ImGui::SliderDragFloat("Gripper Rotation", &controller.target_joint_angles.gripper_rotate, 0.1, 0, 180, is_slider);
+			ImGui::SliderDragFloat("Gripper Control", &controller.target_joint_angles.gripper_control, 0.1, 0, 180, is_slider);
+		
+			ImGui::TreePop();
+		}
 		if (ImGui::TreeNodeEx("Joint Length", treenode_flags)) {
 			ImGui::DragFloat("Arm", &controller.lra_dimension.arm, 0.1, 0);
 			ImGui::DragFloat("Elbow", &controller.lra_dimension.elbow, 0.1, 0);
